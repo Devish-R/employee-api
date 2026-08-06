f963c0847c9722242aaf6c6eb06053e6351fb099
R Devish <your-github-ramamoorthydevish03@gamil.com>
2026-08-06 09:32:06 +0530

    Completed employee curd operations2

---
+++ src/main/java/com/devish/employee_api/config/DataLoader.java
@@ -0,0 +1,24 @@
+package com.devish.employee_api.config;
+
+import com.devish.employee_api.entity.Employee;
+import com.devish.employee_api.repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader {
+
+    @Bean
+    CommandLineRunner loadData(EmployeeRepository repository) {
+
+        return args -> {
+
+            repository.save(new Employee(null, "Devish", "DevOps", 50000));
+            repository.save(new Employee(null, "Rahul", "Cloud", 60000));
+            repository.save(new Employee(null, "Priya", "Testing", 45000));
+            repository.save(new Employee(null, "Arun", "Development", 70000));
+
+        };
+    }
+}
